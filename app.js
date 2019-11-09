9d93c6aac7e093ef64088d71d959c49a875054dd
Wouter Pot <wouter.pot@nielsen.com>
2019-11-09 21:09:12 +0100

    serve react from nodejs

--- app.js
+++ app.js
@@ -1,38 +1,45 @@
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
+var express = require("express")
+var path = require("path")
+var cookieParser = require("cookie-parser")
+var logger = require("morgan")
 
-var indexRouter = require('./routes/index')
-var recipesRouter = require('./routes/recipes')
-var productsRouter = require('./routes/products')
+var recipesRouter = require("./routes/recipes")
+var productsRouter = require("./routes/products")
 
 var app = express()
 
-app.use(logger('dev'))
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")))
+
+app.use(logger("dev"))
 app.use(express.json())
 app.use(
   express.urlencoded({
-    extended: false,
+    extended: false
   })
 )
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")))
 
 // nb: cors settings must be included before other routes
 
-var cors = require('cors')
+var cors = require("cors")
 app.use(cors())
 
-app.use('/recipes', recipesRouter)
-app.use('/products', productsRouter)
-app.use('/', indexRouter)
+app.use("/recipes", recipesRouter)
+app.use("/products", productsRouter)
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/frontend/build/index.html"))
+})
 
 module.exports = app
 
 // start listening
-const port = process.env.PORT || 4000
-app.set('trust proxy', '127.0.0.1')
+const port = process.env.PORT || 5000
+app.set("trust proxy", "127.0.0.1")
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
 })
