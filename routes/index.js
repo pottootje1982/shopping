506ab6397e43eb87f1dbe4987ec5a34d4ef2e437
Wouter Pot <wouter.pot@nielsen.com>
2019-11-09 20:57:43 +0100

    serve react from nodejs

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
-var express = require('express')
+var express = require("express")
 var router = express.Router()
+const path = require("path")
 
-/* GET home page. */
-router.get('/', function(_req, res) {
-  res.render('index', {
-    title: 'Express',
-  })
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/frontend/build/index.html"))
 })
 
 module.exports = router
