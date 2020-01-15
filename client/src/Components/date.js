d194477cd3c664223c3fc4896056857294529527
Wouter Pot <wouter.pot@nielsen.com>
2020-01-15 16:53:20 +0100

    make sure date string is padded with zeroes

--- client/src/Components/date.js
+++ client/src/Components/date.js
@@ -1,5 +1,16 @@
-module.exports = function() {
-  const date = new Date()
-  return `${date.getFullYear()}-${date.getMonth() +
-    1}-${date.getDate()} ${date.toLocaleTimeString("en-GB")}`
+Number.prototype.pad = function(size) {
+  var s = String(this)
+  while (s.length < (size || 2)) {
+    s = "0" + s
+  }
+  return s
+}
+
+module.exports = function(date) {
+  date = date || new Date()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  return `${date.getFullYear()}-${month.pad(2)}-${day.pad(
+    2
+  )} ${date.toLocaleTimeString("en-GB")}`
 }
---
+++ client/src/Components/date.test.js
@@ -0,0 +1,5 @@
+const getDateString = require("./date")
+
+it("Returns date string", async () => {
+  expect(getDateString(new Date(2020, 0, 1))).toEqual("2020-01-01 12:00:00 AM")
+})
